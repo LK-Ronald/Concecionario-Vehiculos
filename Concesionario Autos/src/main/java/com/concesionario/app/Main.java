01284668c1b9cb051a5e3b117710b54224df78cf
LkRonald <rpereirac@unicartagena.co>
2026-04-10 17:40:39 -0500

    fix(pom): add new libreri BCript

---
+++ Concesionario Autos/src/main/java/com/concesionario/app/Main.java
@@ -0,0 +1,10 @@
+package com.concesionario.app;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Sistema Concesionario Autos. Presentado por: " +
+                "Ronald Pereira.");
+
+    }
+}
