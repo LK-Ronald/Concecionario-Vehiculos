16f38e6f72882f1791243e27f9bfaf028f96b787
LkRonald <rpereirac@unicartagena.co>
2026-05-01 08:52:04 -0500

    feat(domain): se agrego el estado de las Persona

---
+++ Concesionario Autos/src/main/java/com/concesionario/domain/enums/PersonaEstatus.java
@@ -0,0 +1,9 @@
+package com.concesionario.domain.enums;
+
+public enum PersonaEstatus {
+
+    ACTIVO,
+    INACTIVO,
+    PENDIENTE,
+    BLOQUEADO;
+}
--- Concesionario Autos/src/main/java/com/concesionario/domain/model/persona/Persona.java
+++ Concesionario Autos/src/main/java/com/concesionario/domain/model/persona/Persona.java
@@ -1,5 +1,6 @@
 package com.concesionario.domain.model.persona;
 
+import com.concesionario.domain.enums.PersonaEstatus;
 import com.concesionario.domain.enums.PersonaRol;
 import com.concesionario.domain.model.persona.valueobject.*;
 import lombok.Value;
@@ -16,6 +17,26 @@ public class Persona {
     PersonaPassword contrasena;
     PersonaFechaNacimiento fechaNacimiento;
     PersonaRol rol;
+    PersonaEstatus estatus;
 
+    public static Persona createPersona(final PersonaDni dni,
+                                        final PersonaNombre nombre,
+                                        final PersonaNombre apellido,
+                                        final PersonaTelefono telefono,
+                                        final PersonaDireccion direccion,
+                                        final PersonaCorreo correo,
+                                        final PersonaPassword contrasena,
+                                        final PersonaFechaNacimiento fechaNacimiento,
+                                        final PersonaRol rol) {
+        return new Persona(dni, nombre, apellido, telefono, direccion, correo, contrasena, fechaNacimiento, rol, PersonaEstatus.PENDIENTE);
+    }
+
+    public Persona activate() {
+        return new Persona(dni, nombre, apellido, telefono, direccion, correo, contrasena, fechaNacimiento, rol, PersonaEstatus.ACTIVO);
+    }
+
+    public Persona deactivate() {
+        return new Persona(dni, nombre, apellido, telefono, direccion, correo, contrasena, fechaNacimiento, rol, PersonaEstatus.INACTIVO);
+    }
 
 }
